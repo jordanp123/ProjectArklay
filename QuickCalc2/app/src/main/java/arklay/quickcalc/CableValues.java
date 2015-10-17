86670ce605675d9d75a7535cbe49195076c44c42
jordan Rose <jordan.rose@uky.edu>
2015-10-17 19:59:11 -0400

    Cable Values
    Cable value selection system updated, If Clauses updated, more updates
    pending.

--- QuickCalc2/app/src/main/java/arklay/quickcalc/CableValues.java
+++ QuickCalc2/app/src/main/java/arklay/quickcalc/CableValues.java
@@ -1,8 +1,6 @@
 package arklay.quickcalc;
 
-/**
- * Created by Jordan on 10/13/2015.
- */
+
 //int x is the AWG with any / removed,Y is 0 if unshielded 1 if shielded,
 // t is zero for portable power cables, t is one for mine power feeder cables.
     //KV tells us the voltage class.
@@ -103,6 +101,89 @@ public  class CableValues {
             real =.015;
             imag=.025;
         } //End of 2KV Unshielded.
+
+
+        else if (x==6 && y==1 && t==0 && KV==2) {
+            real =.552;
+            imag=.038;
+        }
+        else if (x==5 && y==1 && t==0 && KV==2) {
+            real =.438;
+            imag=.036;
+        }
+        else if (x==4 && y==1 && t==0 && KV==2) {
+            real =.347;
+            imag=.035;
+        }
+        else if (x==3 && y==1 && t==0 && KV==2) {
+            real =.275;
+            imag=.034;
+        }
+        else if (x==2 && y==1 && t==0 && KV==2) {
+            real =.218;
+            imag=.033;
+        }
+        else if (x==1 && y==1 && t==0 && KV==2) {
+            real =.173;
+            imag=.033;
+        }
+        else if (x==10 && y==1 && t==0 && KV==2) {
+            real =.134;
+            imag=.032;
+        }
+        else if (x==20 && y==1 && t==0 && KV==2) {
+            real =.107;
+            imag=.031;
+        }
+        else if (x==30 && y==1 && t==0 && KV==2) {
+            real =.085;
+            imag=.030;
+        }
+        else if (x==40 && y==1 && t==0 && KV==2) {
+            real =.068;
+            imag=.029;
+        }
+        else if (x==250 && y==1 && t==0 && KV==2) {
+            real =.057;
+            imag=.030;
+        }
+        else if (x==300 && y==1 && t==0 && KV==2) {
+            real =.048;
+            imag=.029;
+        }
+        else if (x==350 && y==1 && t==0 && KV==2) {
+            real =.041;
+            imag=.029;
+        }
+        else if (x==400 && y==1 && t==0 && KV==2) {
+            real =.036;
+            imag=.028;
+        }
+        else if (x==500 && y==1 && t==0 && KV==2) {
+            real =.029;
+            imag=.028;
+        }
+        else if (x==600 && y==1 && t==0 && KV==2) {
+            real =.024;
+            imag=.027;
+        }
+        else if (x==700 && y==1 && t==0 && KV==2) {
+            real =.021;
+            imag=.027;
+        }
+
+        else if (x==800 && y==1 && t==0 && KV==2) {
+            real =.019;
+            imag=.026;
+        }
+        else if (x==900 && y==1 && t==0 && KV==2) {
+            real =.017;
+            imag=.026;
+        }
+        else if (x==1000 && y==1 && t==0 && KV==2) {
+            real =.015;
+            imag=.026;
+        } //End of 2KV shielded.
 
         Complex result =new Complex(real,imag);
         return result;
